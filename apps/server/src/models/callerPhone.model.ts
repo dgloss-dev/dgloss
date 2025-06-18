0466c30442fd900f02dcc9811fdb4ecff913c7a9
Thiniraxz <thiniragenuka@gmail.com>
2025-06-19 00:19:49 +0530

    Add slot to caller phone and make caller_id-slot unique

--- apps/server/src/models/callerPhone.model.ts
+++ apps/server/src/models/callerPhone.model.ts
@@ -1,15 +1,23 @@
 import { Model, Table, Column, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
 import { Caller } from './caller.model';
+import { CALLER_PHONE_SLOT } from '@workspace/types/enums/callerPhone';
 
 @Table({
   tableName: CallerPhone.CALLER_PHONE_TABLE_NAME,
   timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ['caller_id', 'slot'],
+    },
+  ],
 })
 export class CallerPhone extends Model {
   public static CALLER_PHONE_TABLE_NAME = 'caller_phones';
   public static CALLER_PHONE_ID = 'id';
   public static CALLER_PHONE_CALLER_ID = 'caller_id';
   public static CALLER_PHONE_NUMBER = 'phone_number';
+  public static CALLER_PHONE_SLOT = 'slot';
 
   @Column({
     type: DataType.INTEGER,
@@ -34,6 +42,13 @@ export class CallerPhone extends Model {
     allowNull: false,
   })
   phoneNumber: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(CALLER_PHONE_SLOT)),
+    field: CallerPhone.CALLER_PHONE_SLOT,
+    allowNull: false,
+  })
+  slot: CALLER_PHONE_SLOT;
 
   @BelongsTo(() => Caller)
   caller: Caller;
