6e771fdb876ae67b9631e69f1b2e492f8a970741
codeB1026 <codebeeofficial@gmail.com>
2025-06-25 13:43:47 +0530

    move pageLayout to components folder

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,13 +1,12 @@
-import { PageLayout } from './_components/common/pageLayout';
+import { PageLayout } from '@client/components/common/pageLayout';
 import { useTranslations } from 'next-intl';
-import { TableComponent } from './_components/common/table';
 
 const Page = () => {
   const t = useTranslations();
 
   return (
     <PageLayout>
-      <TableComponent />
+      <h1> DGLOSS</h1>
     </PageLayout>
   );
 };
